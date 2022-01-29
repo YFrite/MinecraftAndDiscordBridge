5008d0907d73a94ed01268ebef33df500623caa9
YFrite <bulatalimbekov5304@gmail.com>
2022-01-29 15:42:22 +0300

    Fix first upload

--- src/main/java/com/yfrite/discordchat/Constants.java
+++ src/main/java/com/yfrite/discordchat/Constants.java
@@ -9,5 +9,5 @@ public class Constants {
 
     public static JDA jda = null;
     public static List<TextChannel> channels = null;
-    public static String DISCORD_API_KEY = "OTM1ODQ2NjEwNzQxNjMzMDU0.YfElEw.WZqYgQxhwUOHjFqWqmGAWSmnydM";
+    public static String DISCORD_API_KEY = "";
 }
